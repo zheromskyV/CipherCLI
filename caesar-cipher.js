7cfe305e86680005b6fad6d05f74cb644bb1d57b
Vladislav Zheromsky <vladislavzheromsky@gmail.com>
2020-10-03 22:58:35 +0300

    feat: add commander

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -1,3 +1,23 @@
-const caesar = require('./src/caesar');
+const program = require('commander');
 
-console.log(caesar('This is secret. Message about "_" symbol!', 7));
+program
+  .storeOptionsAsProperties(false)
+  .version('1.0.0')
+  .option('-s --shift <shift>', 'caesar cipher shift')
+  .option('-i --input <file>', 'input file', process.stdin)
+  .option('-o --output <file>', 'output file', process.stdout)
+  .option('-a --action <action>', 'action: encode/decode')
+  .parse(process.argv);
+
+console.log(program.opts());
+const { shift, action, input, output } = program.opts();
+
+if (shift === undefined || action === undefined) {
+  console.error('"shift" and "action" are required');
+  process.exit(1);
+}
+
+if (action !== 'encode' || action !== 'decode') {
+  console.error('"action" must be "encode" or "decode"');
+  process.exit(1);
+}
